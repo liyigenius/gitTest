fe5cf98263cf1afb4927496e3edc2c9097ba8352
liyigenius <liyigenius@gmail.com>
2017-02-01 14:19:32 +0800

    test

---
+++ a.ly
@@ -0,0 +1,9 @@
+Int a;
+Int b;
+Int all;
+Str c;
+Str d;
+
+a = 1;
+b = 2;
+all = a + b;
--- entry.py
+++ entry.py
@@ -1,1 +1,25 @@
 import sys,os
+
+globalInt = {} # pooling later
+globalStr = {} # pooling later
+keyword=['Int','Str','Func']
+def readSource(fName):
+	pass
+	for i in open(fName).readlines():
+		print i
+		i=i.replace(';','')
+		tokens = i.split()
+		if len(tokens) > 1 :
+			if tokens[0] == 'Int':
+				#add to pool...
+				globalInt[tokens[1]] = 0
+			if tokens[0] == 'Str':
+				globalStr[tokens[1]] = ''
+
+		if i.find('=') >= 0:
+			#parse expr...
+
+
+readSource('a.ly')
+print globalStr
+print globalInt
